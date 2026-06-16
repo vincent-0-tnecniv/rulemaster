4b2c5d15ea7cef278be95c8e061ab6c0fb6ae711
Vincent <ngvincent2020@gmail.com>
2026-06-16 19:48:08 +0800

    Setup Completed

---
+++ src/main/java/net/vincent/rulemaster/RuleMasterClient.java
@@ -0,0 +1,21 @@
+package net.vincent.rulemaster;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuleMasterClient implements ClientModInitializer {
+	public static final String MOD_ID = "rulemaster";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Client Initialized!");
+	}
+}
